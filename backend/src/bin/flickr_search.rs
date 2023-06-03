10dc91e3173c56bd80a7a992d23acebbe0fa2496
Developing Human <developing-human@protonmail.com>
2023-06-03 13:02:30 -0400

    Add helper binary to flickr search an image

---
+++ backend/src/bin/flickr_search.rs
@@ -0,0 +1,19 @@
+use planting_life::flickr;
+use std::env;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let flickr_api_key = env::var("FLICKR_API_KEY").expect("Must define $FLICKR_API_KEY");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Expected exactly two arguments, had: {args:?}");
+        std::process::exit(1);
+    }
+
+    let scientific_name = &args[1];
+    let common_name = &args[2];
+
+    let image = flickr::get_image(scientific_name, common_name, &flickr_api_key).await;
+
+    println!("{image:#?}");
+}
