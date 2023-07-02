8cd556c7a8249098f7d3ae3a23e2e6521d817524
Developing Human <developing-human@protonmail.com>
2023-07-02 17:56:47 -0400

    Remove mock endpoint

--- backend/src/bin/webserver.rs
+++ backend/src/bin/webserver.rs
@@ -1,7 +1,6 @@
 use actix_cors::Cors;
 use actix_web::{get, web, App, HttpServer, Responder};
 use actix_web_lab::sse::{self, ChannelStream, Sender, Sse};
-use futures::executor::block_on;
 use futures::join;
 use futures::stream::{Stream, StreamExt};
 use openai::NativePlant;
@@ -12,7 +11,6 @@ use serde::{self, Deserialize, Serialize};
 use std::boxed::Box;
 use std::env;
 use std::pin::Pin;
-use std::thread;
 use std::time;
 use tracing::{info, warn};
 
@@ -191,100 +189,6 @@ async fn fetch_and_send_citations(sender: &Sender, plant: &NativePlant) {
     }
 }
 
-#[get("/plants_mock")]
-async fn fetch_plants_handler_mock(
-    web::Query(payload): web::Query<FetchRequest>,
-) -> impl Responder {
-    info!("Mock: {:?}", payload);
-
-    let (sender, stream): (Sender, Sse<ChannelStream>) = sse::channel(2);
-
-    // Pointing all mock attributions to the same image while I wire up something reasonable
-    let image = flickr::Image {
-        scientific_name: String::from("Asclepias incarnata"),
-        title: String::from("Milkweed"),
-        thumbnail_url: String::from("https://live.staticflickr.com/71/175851524_04904b8383_q_d.jpg"),
-        card_url: String::from("https://live.staticflickr.com/71/175851524_04904b8383_z_d.jpg"),
-        original_url: String::from("https://www.flickr.com/photos/salim/175851524/in/photolist-LYSddJ-do77yy-gxh1X-gxhhQ-gxhwQ-29aXjBh-gxiXt-gxjzi-2gTsfpW-2nu6TdF-t7vKB-gQpY2t-2jxkYNQ-Vxf8qd-yn9kQ6-25wQste-2jxgKNA-rpQqA4-BsgvHn-pPbpVq-dwwp6P-dwBTEW-66cBaE-7iwAYm-7dcY49-6AaZnj-6AaZQw-5amQLW-6A6R9F-4PhsgS-2m57JQa-tZkSa-dwBUjf-dwBW3f-69CtD-8Qzoco-8Qzo6U-4ayh5W-5xnfwM-3ahGQv-ffDEDm-BzyB2B-7e9bKZ-AC367L-dhJw92-ybwhs-6K9AGp-6K9AvT"),
-        author: String::from("Salim Virji"),
-        license: String::from("CC BY-SA 2.0"),
-        license_url: String::from("https://creativecommons.org/licenses/by-sa/2.0/"),
-    };
-
-    let image_json = serde_json::to_string(&image).unwrap();
-
-    thread::spawn(move || {
-        for mut plant in build_mock_plants() {
-            // Remove image to let it load a moment later.
-            let image_url = plant.image_url;
-            plant.image_url = None;
-
-            let plant_json = serde_json::to_string(&plant).unwrap();
-
-            block_on(sender.send(sse::Data::new(plant_json))).unwrap();
-            thread::sleep(time::Duration::from_millis(250));
-
-            block_on(
-                sender.send(
-                    sse::Data::new(format!("{}::{}", plant.scientific, image_url.unwrap()))
-                        .event("image_url"),
-                ),
-            )
-            .unwrap();
-
-            block_on(sender.send(sse::Data::new(image_json.clone()).event("image"))).unwrap();
-            thread::sleep(time::Duration::from_millis(500));
-        }
-
-        block_on(sender.send(sse::Data::new("").event("close"))).unwrap();
-    });
-
-    stream
-        .with_keep_alive(time::Duration::from_secs(1))
-        .customize()
-        .insert_header(("X-Accel-Buffering", "no"))
-}
-
-fn build_mock_plants() -> impl Iterator<Item = NativePlant> {
-    vec![
-        NativePlant {
-            common: "Wild Columbine".to_string(),
-            scientific: "Aquilegia canadensis".to_string(),
-            bloom: Some("Spring to early summer".to_string()),
-            description: Some("This plant is a favorite of hummingbirds and supports the Columbine Duskywing butterfly caterpillar.".to_string()),
-            image_url: Some("https://live.staticflickr.com/5031/7238526710_80bf103077_q.jpg".to_string()),
-        },
-        NativePlant {
-            common: "Swamp Milkweed".to_string(),
-            scientific: "Asclepias incarnata".to_string(),
-            bloom: Some("Summer".to_string()),
-            description: Some("This plant is a favorite of hummingbirds and supports the Columbine Duskywing butterfly caterpillar.".to_string()),
-            image_url: Some("https://live.staticflickr.com/3126/3147197425_4e9ac1e2ca_q.jpg".to_string()),
-        },
-        NativePlant {
-            common: "Joe Pye Weed".to_string(),
-            scientific: "Eutrochium purpureum".to_string(),
-            bloom: Some("Late summer to fall".to_string()),
-            description: Some("This plant is a favorite of hummingbirds and supports the Columbine Duskywing butterfly caterpillar.".to_string()),
-            image_url: Some("https://live.staticflickr.com/3862/15215414361_9f659f6f52_q.jpg".to_string()),
-        },
-        NativePlant {
-            common: "Blue Flag Iris".to_string(),
-            scientific: "Iris versicolor".to_string(),
-            bloom: Some("Late spring to early summer".to_string()),
-            description: Some("This plant is a favorite of hummingbirds and supports the Columbine Duskywing butterfly caterpillar.".to_string()),
-            image_url: Some("https://live.staticflickr.com/65535/50623901946_1c37f69ccd_q.jpg".to_string()),
-        },
-        NativePlant {
-            common: "Cardinal Flower".to_string(),
-            scientific: "Lobelia cardinalis".to_string(),
-            bloom: Some("Late summer to early fall".to_string()),
-            description: Some("This plant is a favorite of hummingbirds and supports the Columbine Duskywing butterfly caterpillar.".to_string()),
-            image_url: Some("https://live.staticflickr.com/6174/6167236354_c7e9771f00_q.jpg".to_string()),
-        }
-    ].into_iter()
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     tracing_subscriber::fmt::init();
@@ -295,10 +199,7 @@ async fn main() -> std::io::Result<()> {
             .allowed_origin("https://planting.life")
             .allowed_methods(vec!["GET"]);
 
-        App::new()
-            .wrap(cors)
-            .service(fetch_plants_handler)
-            .service(fetch_plants_handler_mock)
+        App::new().wrap(cors).service(fetch_plants_handler)
     })
     .bind("0.0.0.0:8080")?
     .run()
