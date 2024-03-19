f006fea5db16b842c490839c4723acb70aec66a4
Developing Human <developing-human@protonmail.com>
2024-03-18 22:57:54 -0400

    Cleanup more backend streaming

--- backend/src/controllers/plants.rs
+++ backend/src/controllers/plants.rs
@@ -1,20 +1,12 @@
 use actix_web::{get, web, HttpResponse, Responder};
 use mockall_double::double;
-use std::{pin::Pin, time::Duration};
-
-use actix_web_lab::sse::{self, ChannelStream, Sender, Sse};
-use futures::{channel::mpsc, Stream, StreamExt};
 use serde::{Deserialize, Serialize};
 use tracing::log::{info, warn};
 
 #[double]
 use crate::database::Database;
 use crate::{
-    app::PlantingLifeApp,
-    domain::*,
-    highlights::Highlights,
-    hydrator::{self, Hydrator},
-    selector::Selector,
+    app::PlantingLifeApp, domain::*, highlights::Highlights, hydrator::Hydrator, selector::Selector,
 };
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -123,127 +115,6 @@ impl PlantController {
 
         Ok(valid_zip)
     }
-
-    async fn hydrate_and_send_plants(
-        &'static self,
-        payload: Option<PlantsStreamRequest>,
-        plant_stream: Pin<Box<dyn Stream<Item = Plant> + Send>>,
-        frontend_sender: &Sender,
-        plants_from_db: bool,
-    ) {
-        let (mut plant_sender, mut plant_receiver) = mpsc::unbounded();
-
-        actix_web::rt::spawn(async move {
-            self.hydrator
-                .hydrate_plants(plant_stream, &mut plant_sender)
-                .await;
-        });
-
-        let mut all_plants = vec![];
-        let mut saved_plants = vec![];
-        while let Some(hydrated_plant) = plant_receiver.next().await {
-            if hydrated_plant.plant.scientific == hydrator::ALL_PLANTS_HYDRATING_MARKER {
-                send_event(frontend_sender, "allPlantsLoaded", "").await;
-
-                continue;
-            }
-
-            send_plant(frontend_sender, &hydrated_plant.plant).await;
-
-            if hydrated_plant.plant.done_loading {
-                all_plants.push(hydrated_plant.plant.clone());
-
-                if hydrated_plant.updated {
-                    //plants_to_save.push(hydrated_plant.plant.clone());
-                    match self.db.save_plant(&hydrated_plant.plant).await {
-                        Ok(plant) => saved_plants.push(plant.clone()),
-                        Err(e) => warn!(
-                            "Failed to save {} due to {e}",
-                            hydrated_plant.plant.scientific
-                        ),
-                    }
-                }
-            }
-        }
-
-        // We only need to cache the query results if these results aren't from the database
-        // When they are from the database, we know its already there.
-        if let Some(payload) = payload {
-            if !plants_from_db {
-                self.db
-                    .save_query_results(
-                        &payload.zip,
-                        &payload.moisture,
-                        &payload.shade,
-                        all_plants,
-                        saved_plants,
-                    )
-                    .await;
-            }
-        }
-    }
-
-    /// Streams one plant back by scientific name.  Uses a stream because it may
-    /// need to be populated still and it should load incrementally.  Uses
-    /// scientific name rather than id because id makes it trivial to download
-    /// the entire database :)
-    async fn stream_by_scientific_name(
-        &'static self,
-        id: String,
-    ) -> Result<impl Responder, actix_web::Error> {
-        info!("stream_by_scientific_name {id}");
-
-        let (frontend_sender, stream): (Sender, Sse<ChannelStream>) = sse::channel(10);
-
-        // The real work is done in a new thread so the connection to the front end can stay open.
-        actix_web::rt::spawn(async move {
-            let plant = self.db.get_plant_by_scientific_name(&id).await;
-            if plant.is_none() {
-                warn!("Couldn't find plant with id: {id}");
-                return;
-            }
-
-            // Create a stream to interface nicely with hydrate/send function
-            // Unwrap is safe due to is_none check above.
-            let stream = Box::pin(futures::stream::iter(vec![plant.unwrap()]));
-
-            self.hydrate_and_send_plants(None, stream, &frontend_sender, true)
-                .await;
-
-            send_event(&frontend_sender, "close", "").await;
-        });
-
-        Ok(stream
-            .with_keep_alive(Duration::from_secs(1))
-            .customize()
-            .insert_header(("X-Accel-Buffering", "no")))
-    }
-}
-
-async fn send_plant(sender: &Sender, plant: &Plant) {
-    let json = serde_json::to_string(&plant).expect("plant should serialize");
-
-    send_event(sender, "plant", &json).await;
-}
-
-async fn send_event(sender: &Sender, event: &str, message: &str) {
-    let data = sse::Data::new(message).event(event);
-
-    match sender.send(data).await {
-        Ok(_) => {}
-        Err(_) => warn!("Error sending [{}] with message [{}]", event, message),
-    }
-}
-
-#[get("/plants/stream/{scientific_name}")]
-async fn plants_stream_by_scientific_name_handler(
-    id: web::Path<String>,
-    app: web::Data<&'static PlantingLifeApp>,
-) -> Result<impl Responder, actix_web::Error> {
-    panic!("shouldn't use");
-    app.plant_controller
-        .stream_by_scientific_name(id.to_string())
-        .await
 }
 
 #[get("/plants/{id}")]
