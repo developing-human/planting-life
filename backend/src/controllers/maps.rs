e0e4becd5ffe698dd9879a59580402dd82066e5c
Developing Human <developinghuman@protonmail.com>
2025-11-29 21:37:50 -0500

    add forgotten maps controller

---
+++ backend/src/controllers/maps.rs
@@ -0,0 +1,44 @@
+use std::env;
+
+use actix_web::{get, web, Responder};
+use mockall_double::double;
+use tracing::log::info;
+
+use crate::app::PlantingLifeApp;
+
+#[double]
+use crate::database::Database;
+
+pub struct MapsController {
+    pub db: &'static Database,
+}
+
+impl MapsController {
+    pub fn new(db: &'static Database) -> Self {
+        Self { db }
+    }
+
+    async fn get_api_key(&self) -> impl Responder {
+        info!("fetching maps api key");
+
+        let uri = "/maps/api-key";
+        self.db.update_request_count(uri).await;
+        let monthly_count = self.db.get_monthly_request_count(uri).await;
+        info!("monthly_count is {monthly_count}");
+
+        if monthly_count >= 10_000 {
+            return actix_web::HttpResponse::ServiceUnavailable().finish();
+        }
+
+        if let Ok(api_key) = env::var("PLANTING_LIFE_MAPS_API_KEY") {
+            actix_web::HttpResponse::Ok().body(api_key)
+        } else {
+            actix_web::HttpResponse::NotFound().finish()
+        }
+    }
+}
+
+#[get("/maps/api-key")]
+async fn maps_api_key_handler(app: web::Data<&'static PlantingLifeApp>) -> impl Responder {
+    app.maps_controller.get_api_key().await
+}
